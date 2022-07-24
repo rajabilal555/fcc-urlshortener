d05ada0ac266f7299508aa82dbd90b47e43298a8
Bilal Pervez <rajabilal555@gmail.com>
2022-07-24 21:41:28 +0500

    add get api 😅 and add url check

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 require("dotenv").config();
+const URL = require("url").URL;
+const dns = require("node:dns").promises;
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -30,29 +32,61 @@ app.get("/api/hello", function (req, res) {
 	res.json({ greeting: "hello API" });
 });
 
-app.post("/api/shorturl", function (req, res) {
-	console.log(req.body.url);
-	ShortenerUrl.findOne({ original_url: req.body.url }, function (err, doc) {
-		if (!doc) {
-			var newUrl = new ShortenerUrl();
-			newUrl.original_url = req.body.url;
-			newUrl.shortened_url = Math.floor(Math.random() * 1000000);
-			newUrl.save(function (err, doc) {
-				console.log(doc);
-				res.json({
-					original_url: doc.original_url,
-					short_url: doc.shortened_url,
-				});
-			});
-		} else {
-			res.json({
-				original_url: doc.original_url,
-				short_url: doc.shortened_url,
-			});
+app.get("/api/shorturl/:url", function (req, res) {
+	ShortenerUrl.findOne(
+		{ shortened_url: req.params.url },
+		function (err, data) {
+			if (err) {
+				res.json({ error: "invalid url" });
+			} else {
+				res.redirect(data.original_url);
+			}
 		}
-	});
+	);
+});
+
+app.post("/api/shorturl", async function (req, res) {
+	console.log(req.body.url);
+	if (await isValidUrl(req.body.url)) {
+		ShortenerUrl.findOne(
+			{ original_url: req.body.url },
+			function (err, doc) {
+				if (!doc) {
+					var newUrl = new ShortenerUrl();
+					newUrl.original_url = req.body.url;
+					newUrl.shortened_url = Math.floor(Math.random() * 1000000);
+					newUrl.save(function (err, doc) {
+						console.log(doc);
+						res.json({
+							original_url: doc.original_url,
+							short_url: doc.shortened_url,
+						});
+					});
+				} else {
+					res.json({
+						original_url: doc.original_url,
+						short_url: doc.shortened_url,
+					});
+				}
+			}
+		);
+	} else {
+		res.json({ error: "invalid url" });
+	}
 });
 
 app.listen(port, function () {
 	console.log(`Listening on port ${port}`);
 });
+
+async function isValidUrl(urlString) {
+	try {
+		const url = new URL(urlString);
+		let res = await dns.lookup(url.hostname);
+		if (res) {
+			return true;
+		}
+	} catch (error) {
+		return false;
+	}
+}
